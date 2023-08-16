e6379f78ec99453e67930e1e644067f761a0362f
amousavigourabi <28668597+amousavigourabi@users.noreply.github.com>
2023-08-16 22:23:34 +0200

    Invert diff condition

--- commit.sh
+++ commit.sh
@@ -2,7 +2,7 @@
 if [ "$2" == "true" ]
 then
   git commit --allow-empty -m "$1" -m "Empty"
-elif git diff --cached --quiet --exit-code
+elif ! git diff --cached --quiet --exit-code
 then
   git commit -m "$1"
 fi
