562d61af3a961cfc14c42c54e3885281cea2762a
amousavigourabi <28668597+amousavigourabi@users.noreply.github.com>
2023-08-16 22:25:19 +0200

    Merge conditionals

--- commit.sh
+++ commit.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
-if [ "$2" == "true" ]
+if [ "$2" == "true" ] || ! git diff --cached --quiet --exit-code
 then
-  git commit --allow-empty -m "$1" -m "Empty"
-elif ! git diff --cached --quiet --exit-code
-then
-  git commit -m "$1"
+  git commit --allow-empty -m "$1"
 fi
 
