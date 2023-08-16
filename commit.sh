ce94726d38b61a1d15405e5a52b78f6a605431c0
amousavigourabi <28668597+amousavigourabi@users.noreply.github.com>
2023-08-16 22:21:20 +0200

    Split commit script into two branches

--- commit.sh
+++ commit.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-if [ "$2" == "true" ] || git diff --cached --quiet --exit-code
+if [ "$2" == "true" ]
 then
-	git commit --allow-empty -m "$1"
+  git commit --allow-empty -m "$1" -m "Empty"
+elif git diff --cached --quiet --exit-code
+then
+  git commit -m "$1"
 fi
 
