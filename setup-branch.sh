789da6daea5bd1e5388b725030530bb9bb3cf7db
amousavigourabi <28668597+amousavigourabi@users.noreply.github.com>
2023-08-16 21:04:22 +0200

    Add forgotten switch when the branch does exist

--- setup-branch.sh
+++ setup-branch.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 git fetch --all
-if ! git show-ref --verify --quiet "refs/heads/$1"
+if git rev-parse --quiet --verify "$1" >/dev/null
 then
+  git switch $1
+else
   git switch --orphan $1
 fi
