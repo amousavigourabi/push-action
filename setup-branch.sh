e2ca5a57d823dd0a902ca1f7c0a39995911d0299
amousavigourabi <28668597+amousavigourabi@users.noreply.github.com>
2023-08-17 00:33:09 +0200

    Add debug ls

--- setup-branch.sh
+++ setup-branch.sh
@@ -2,13 +2,18 @@
 git fetch --all --quiet
 if git rev-parse --quiet --verify "origin/$1" >/dev/null
 then
+  ls -la
   git stash --include-untracked
   work_branch=$(git symbolic-ref --short HEAD)
   git switch $1
+  ls -la
   git checkout "$work_branch" -- .
+  ls -la
   git merge --squash --strategy-option=theirs --allow-unrelated-histories stash
+  ls -la
   git stash drop
   git reset HEAD -- .
+  ls -la
 else
   git switch --orphan $1
 fi
