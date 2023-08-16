d0bbebf5b5beb6526972c8bad2546e48b6e9f400
amousavigourabi <28668597+amousavigourabi@users.noreply.github.com>
2023-08-17 01:42:50 +0200

    Use merge instead of checkout

--- setup-branch.sh
+++ setup-branch.sh
@@ -2,18 +2,13 @@
 git fetch --all --quiet
 if git rev-parse --quiet --verify "origin/$1" >/dev/null
 then
-  ls
   git stash --include-untracked
   work_branch=$(git symbolic-ref --short HEAD)
   git switch $1
-  ls
-  git checkout "$work_branch" -- .
-  ls
+  git merge --squash --strategy-option=theirs --allow-unrelated-histories $work_branch
   git merge --squash --strategy-option=theirs --allow-unrelated-histories stash
-  ls
   git stash drop
   git reset HEAD -- .
-  ls
 else
   git switch --orphan $1
 fi
