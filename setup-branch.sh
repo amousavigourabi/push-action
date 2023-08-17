296a93efe6d19454af19b153437f87e21cea44fe
amousavigourabi <28668597+amousavigourabi@users.noreply.github.com>
2023-08-17 02:34:43 +0200

    debug statements

--- setup-branch.sh
+++ setup-branch.sh
@@ -2,12 +2,22 @@
 git fetch --all --quiet
 if git rev-parse --quiet --verify "origin/$1" >/dev/null
 then
+  echo "aaaaaaaa"
+  ls -la
   git stash --include-untracked
   stash_ref=$(git stash create)
   work_branch=$(git symbolic-ref --short HEAD)
+  echo "eeeeeeee"
+  ls -la
   git switch $1
+  echo "iiiiiiii"
+  ls -la
   git merge --squash --strategy-option=theirs --allow-unrelated-histories $work_branch
+  echo "oooooooo"
+  ls -la
   git stash apply "$stash_ref"
+  echo "uuuuuuuu"
+  ls -la
   git reset HEAD -- .
 else
   git switch --orphan $1
