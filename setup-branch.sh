f006c20d2969305fe82afb47884a2158e50e8618
amousavigourabi <28668597+amousavigourabi@users.noreply.github.com>
2023-08-17 03:07:09 +0200

    Stash and merge for new branches too

--- setup-branch.sh
+++ setup-branch.sh
@@ -1,24 +1,14 @@
 #!/bin/sh
 git fetch --all --quiet
+git stash --include-untracked
+stash_ref=$(git stash create)
+work_branch=$(git symbolic-ref --short HEAD)
 if git rev-parse --quiet --verify "origin/$1" >/dev/null
 then
-  echo "aaaaaaaa"
-  ls -la
-  git stash --include-untracked
-  stash_ref=$(git stash create)
-  work_branch=$(git symbolic-ref --short HEAD)
-  echo "eeeeeeee"
-  ls -la
   git switch $1
-  echo "iiiiiiii"
-  ls -la
-  git merge --squash --strategy-option=theirs --allow-unrelated-histories $work_branch
-  echo "oooooooo"
-  ls -la
-  git stash apply "$stash_ref"
-  echo "uuuuuuuu"
-  ls -la
-  git reset HEAD -- .
 else
   git switch --orphan $1
 fi
+git merge --squash --strategy-option=theirs --allow-unrelated-histories $work_branch
+git stash apply "$stash_ref"
+git reset HEAD -- .
